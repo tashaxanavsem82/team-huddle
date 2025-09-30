d258d094e4830cfb570641d0711d5e7674b8407c
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-30 09:49:02 -0500

    Added redirect to the main page after creating a room

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ document.getElementById('create-room').addEventListener('click', function() {
     console.log('Room Created!');
     alert('Room has been successfully created!');
     // Optionally redirect or updated UI
+    window.location.href = '/main';
 });
